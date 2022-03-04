4844bbf983be6be85065f9cbe8da8985663b038a
Patrick Cash <patrickcash@gmail.com>
2022-03-04 18:21:16 -0500

    display all of the durrent pins and color bu username

--- frontend/src/components/map/PinMap.jsx
+++ frontend/src/components/map/PinMap.jsx
@@ -1,19 +1,41 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./pinMap.css"
-
-import Map, {Marker, Popup} from "react-map-gl";
-import { Star } from "@material-ui/icons";
-
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import axios from "axios";
+import Map, {Marker, Popup} from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import { format } from "timeago.js";
+
+
+
 export default function PinMap() {
+  const [pins, setPins] = useState([]);
+  const [currentUsername, setCurrentUsername] = useState("patrick");
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [title, setTitle] = useState(null);
   const [review, setReview] = useState(null);
   const [star, setStar] = useState(0);
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+  };
+
   const handleSubmit = async (e) => {
 
   }
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const pins = await axios.get("/pins");
+        setPins(pins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
 
   return (
     <Map
@@ -26,34 +48,46 @@ export default function PinMap() {
       mapStyle="mapbox://styles/mapbox/outdoors-v11"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
     >
-        <Marker latitude={37.8} longitude={-122.4} color="red" />
-        <Popup
-          latitude={37.8}
-          longitude={-122.4}
-          closeButton={true}
-          closeOnClick={false}
-          onClose={() => {}}
-          anchor="left"
-        >
-          <div className="popup">
-            <label>Place</label>
-            <h4 className="place">{title}</h4>
-            <label>Review</label>
-            <p className="desc">{review}</p>
-            <label>Rating</label>
-            <div className="stars">
-              {Array(star).fill(<Star className="star" />)}
-            </div>
-            <label>Information</label>
-            <span className="username">
-              Created by <b>user</b>
-            </span>
-            <span className="date">today</span>
-          </div>
-        </Popup>
-        <Popup
-          latitude={37.8}
-          longitude={-100.4}
+      {pins.map(pin => (
+        <>
+          <Marker key={"marker"+pin._id} latitude={pin.lat} longitude={pin.long} >
+            <Room
+                style={{color: currentUsername === pin.username ? "red" : "blue", cursor: "pointer",}}
+                onClick={() => handleMarkerClick(pin._id, pin.lat, pin.long)}
+              />
+          </Marker>
+          {pin._id === currentPlaceId && (
+            <Popup
+              key={"popup"+pin._id} 
+              latitude={pin.lat}
+              longitude={pin.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => {}}
+              anchor="left"
+            >
+              <div className="popup">
+                <label>Place</label>
+                <h4 className="place">{pin.title}</h4>
+                <label>Review</label>
+                <p className="desc">{pin.review}</p>
+                <label>Rating</label>
+                <div className="stars">
+                  {Array(pin.rating).fill(<Star className="star" />)}
+                </div>
+                <label>Information</label>
+                <span className="username">
+                  Created by <b>{pin.username}</b>
+                </span>
+                <span className="date">{format(pin.createdAt)}</span>
+              </div>
+            </Popup>
+          )}
+        </>
+      ))}
+        {/* <Popup
+          latitude={pin.lat}
+          longitude={pin.long}
           closeButton={true}
           closeOnClick={false}
           onClose={() => {}}
@@ -83,7 +117,7 @@ export default function PinMap() {
               </button>
             </form>
           </div>
-        </Popup>
+        </Popup> */}
     </Map>
   )
 }
