a25f29a2c54fd57404ecdda25d56e4497cd3f467
Patrick Cash <patrickcash@gmail.com>
2022-03-30 17:02:10 -0400

    heroku setup

--- backend/index.js
+++ backend/index.js
@@ -22,11 +22,13 @@ app.use("/users", userRoute);
 // Serve static assets
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
     });
+} else {
+    app.get('*', (req, res) => res.send('Please set to Production mode'));
 }
 
 const port = process.env.PORT || 8800;
